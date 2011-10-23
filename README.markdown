bc4095e43d4b5b40c2092ea85832248ba5b39c6f
Alex Kantchelian <alex.kantchelian@gmail.com>
2011-10-22 18:00:52 -0700

    Cython module

--- README.markdown
+++ README.markdown
@@ -1,10 +1,10 @@
 # Summary
 
-This is a simple implementation of discrete hidden markov models for Python > 2.5. It is currently written in pure python but will eventually be cythonized for cutting edge performances.
+This is a simple implementation of discrete hidden markov models for Python > 2.5. Both pure Python and Cython implementations are maintened. The Cython module is 10x (learning) to 30x (inference) faster than the Python implementation, but you'll need to compile it.
 
 # Features
 
-* Supervised (explicit) learning - no regularization
+* Supervised learning - no regularization
 * Viterbi inference
 
 The following features are out-of-scope:
@@ -12,13 +12,32 @@ The following features are out-of-scope:
 * Unsupervised learning (Baum-Welch)
 * Regularization
 
+# Building the Cython module
+
+Assuming a working installation of Cython, running:
+	
+	python setup.py build_ext --inplace
+
+will build the chmmpy module.
+
 # Basic usage
 
 	>>> from hmmpy import hmm
+	>>> from chmmpy import hmm as chmm
+	
 	>>> m=hmm(2, 2)
+	>>> cm=chmm(2,2)
+	
 	>>> observations=[[0,0,0,0,0,1,1,1,1,1,0,1,0,0,0,1,0,1,1,1,1],[0,0,0,0,1,0,1,1,0,1,1,0,0,1,0,0,1,1,1,1,0,0,1,0,0]]
 	>>> ground_truths=[[0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1],[0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0]]
-	>>> m.explicit_learn(observations, ground_truths)
+	
+	>>> m.learn(observations, ground_truths)
+	>>> cm.learn(observations, ground_truths)
+	
 	>>> m.viterbi(observations[1])
 	([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0], -21.944735122525493)
+	
+	>>> cm.viterbi(observations[1])
+	([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0], -21.944734573364258)
+
 
--- chmmpy.pyx
+++ chmmpy.pyx
@@ -1,8 +1,25 @@
-from __future__ import division
-from math import log
+from cpython cimport bool
+from libc.stdlib cimport malloc, calloc, free
+from libc.math cimport log
 
-class hmm:
-    def __init__(self, nStates, nObs):
+cdef float MIN_FLOAT = float('-inf')
+
+cdef float csum(float* tab, int n):
+        cdef int i=0
+        cdef float res=0
+        while i<n:
+                res+=tab[i]
+                i+=1
+        return res
+
+cdef class hmm:
+    cdef public int nStates, nObs
+    cdef bool logdomain
+    cdef float* pi
+    cdef float** t
+    cdef float** e
+
+    def __init__(self, int nStates, int nObs):
         """HMM constructor.
         
         Parameters
@@ -11,14 +28,20 @@ class hmm:
             Number of hidden states.
         nObs: non-negative integer
             Number of possible observed values."""
-        self.pi=[0]*nStates
-        self.t=[[0]*nStates for i in range(nStates)]
-        self.e=[[0]*nObs for i in range(nStates)]
+        self.pi= <float*> calloc(nStates, sizeof(float))
+        self.t= <float**> malloc(nStates*sizeof(float*))
+        self.e= <float**> malloc(nObs*sizeof(float*))
+        cdef int i=0
+        for i in range(nStates):
+                self.t[i]= <float*> calloc(nStates, sizeof(float))
+                self.e[i]= <float*> calloc(nObs, sizeof(float))
+                i+=1
+        
         self.nStates=nStates
         self.nObs=nObs
         self.logdomain=False
 
-    def learn(self, observations, ground_truths):
+    def learn(self, list observations, list ground_truths):
         """Learns from a list of observation sequences and their associated ground truth.
 
         Parameters
@@ -27,50 +50,51 @@ class hmm:
             List of observed sequences.
         ground_truths: list of list of integers in {0, ..., nStates-1}
             Associated list of ground truths.""" 
-        assert(len(observations)==len(ground_truths))
         self.__init__(self.nStates, self.nObs)
         N=len(observations)
-        for i in range(N):
+        cdef int i, j
+        for i in xrange(N):
             o=observations[i]
             t=ground_truths[i]
-            assert(len(o)==len(t))
 
             self.pi[t[0]]+=1
             for j in range(len(t)-1):
-                self.t[t[j]][t[j+1]]+=1
-                self.e[t[j]][o[j]]+=1
+                self.t[t[j]][t[j+1]]+=1 #possible loss of precision
+                self.e[t[j]][o[j]]+=1   #rewrite with int** types
             j+=1
             self.e[t[j]][o[j]]+=1
 
+        cdef float Zt, Ze
         for i in range(self.nStates):
             self.pi[i]/=N
-            Zt=sum(self.t[i])
-            Ze=sum(self.e[i])
+            Zt= csum(self.t[i], self.nStates)
+            Ze= csum(self.e[i], self.nObs)
             for j in range(self.nStates):
-                self.t[i][j]/=max(1, Zt)
+                self.t[i][j]/=max(1., Zt)
             for j in range(self.nObs):
-                self.e[i][j]/=max(1, Ze)
+                self.e[i][j]/=max(1., Ze)
 
     def __convert_to_log(self):
         """Convers the internal probability tables in the log domain."""
+        cdef int i, j
         for i in range(self.nStates):
             if self.pi[i]>0:
                 self.pi[i]=log(self.pi[i])
             else:
-                self.pi[i]=float('-inf')
+                self.pi[i]=MIN_FLOAT
             for j in range(self.nStates):
                 if self.t[i][j]>0:
                     self.t[i][j]=log(self.t[i][j])
                 else:
-                    self.t[i][j]=float('-inf')
+                    self.t[i][j]=MIN_FLOAT
             for j in range(self.nObs):
                 if self.e[i][j]>0:
                     self.e[i][j]=log(self.e[i][j])
                 else:
-                    self.e[i][j]=float('-inf')
+                    self.e[i][j]=MIN_FLOAT
         self.logdomain=True
 
-    def viterbi(self, observation):
+    def viterbi(self, list observation):
         """Viterbi inference of the highest likelihood hidden states sequence given the observations. Time complexity is O(|observation|*nStates^2).
         
         Parameters
@@ -83,38 +107,43 @@ class hmm:
             Highest likelihood infered sequence of hidden states.
         loglike: negative float
             Loglikelihood of the model for that sequence."""
-        N=len(observation)
-        tab=[[0]*self.nStates for i in range(N)]
-        backtrack=[[-1]*self.nStates for i in range(N)]
+        cdef int N=len(observation)
+        cdef float** tab = <float**> malloc(N*sizeof(float*))
+        cdef int** backtrack = <int**> malloc(N*sizeof(float*))
+        cdef int i, j, s, smax
+        cdef float maxval, cs, llike
+        for i in xrange(N):
+                tab[i] = <float*> malloc(self.nStates*sizeof(float))
+                backtrack[i] = <int*> malloc(self.nStates*sizeof(int))
+        
         if not self.logdomain:
             self.__convert_to_log()
 
         for i in range(self.nStates):
             tab[0][i]=self.e[i][observation[0]]+self.pi[i]
         
-        for i in range(1,N):
+        for i in xrange(1,N):
             for j in range(self.nStates):
                 smax=-1
-                maxval=float('-inf')
+                maxval=MIN_FLOAT
                 for s in range(self.nStates):
                     cs=tab[i-1][s]+self.t[s][j]
                     if cs>maxval:
                         smax=s
                         maxval=cs
-                assert(smax>-1 and smax<self.nStates)
                 tab[i][j]=self.e[j][observation[i]]+maxval
                 backtrack[i][j]=smax
 
         smax=-1
-        llike=float('-inf')
+        llike=MIN_FLOAT
         for s in range(self.nStates):
             if llike<tab[N-1][s]:
                 llike=tab[N-1][s]
                 smax=s
 
-        best=[-1]*N
-        best[-1]=smax
-        for i in range(N-2, -1, -1):
+        best=[0]*N
+        best[N-1]=smax
+        for i in xrange(N-2, -1, -1):
             best[i]=backtrack[i+1][best[i+1]]
 
         return best, llike
